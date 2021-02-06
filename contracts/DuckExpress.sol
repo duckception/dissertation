f728edde0f971d1907e0d6d928768461088e6814
Daniel Izdebski <spidiritsu@gmail.com>
2021-02-06 20:31:44 +0100

    Add simple contract

---
+++ contracts/DuckExpress.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity 0.7.3;
+pragma experimental ABIEncoderV2;
+
+contract DuckExpress {
+    function works() public pure returns (bool) {
+        return true;
+    }
+}
