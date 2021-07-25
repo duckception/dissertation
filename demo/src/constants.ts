a73a88307b6c6b0f2352027da05af96e6562df1c
Daniel Izdebski <spidiritsu@gmail.com>
2021-07-25 16:11:34 +0200

    Set up useDApp

---
+++ demo/src/constants.ts
@@ -0,0 +1,3 @@
+const INFURA_API_KEY = ""
+
+export { INFURA_API_KEY }
--- demo/src/index.tsx
+++ demo/src/index.tsx
@@ -1,10 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App"
+import { ChainId, DAppProvider, Config } from '@usedapp/core'
+import { INFURA_API_KEY } from './constants'
+
+const config: Config = {
+  readOnlyChainId: ChainId.Rinkeby,
+  readOnlyUrls: {
+    [ChainId.Mainnet]: `https://rinkeby.infura.io/v3/${INFURA_API_KEY}`,
+  },
+}
 
 ReactDOM.render(
   <React.StrictMode>
+    <DAppProvider config={config}>
       <App />
+    </DAppProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
