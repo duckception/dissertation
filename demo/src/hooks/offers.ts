3210b840d320ff49f5e3dd180be3e7efc74161ef
Daniel Izdebski <spidiritsu@gmail.com>
2021-07-27 22:42:32 +0200

    Make offers return offer and its status

--- demo/src/hooks/offers.ts
+++ demo/src/hooks/offers.ts
@@ -2,12 +2,29 @@ import { useContractCall } from "@usedapp/core";
 import { duckExpressInterface } from ".";
 import { DUCK_EXPRESS_CONTRACT_ADDRESS } from "../constants"
 
+export interface OffersOffer {
+  offerHash: string
+  offerStatus: number
+}
+
 export function useOffers() {
-  const offers: string[] = useContractCall({
+  const rawOffers = useContractCall({
     abi: duckExpressInterface,
     address: DUCK_EXPRESS_CONTRACT_ADDRESS,
     method: "offers",
     args: [],
   }) ?? [];
+
+  const offers: OffersOffer[] = []
+
+  for (let i = 0; i < rawOffers.length; i++) {
+    const element = rawOffers[i][0];
+
+    offers.push({
+      offerHash: element[0],
+      offerStatus: element[1]
+    })
+  }
+  
   return offers;
 }
