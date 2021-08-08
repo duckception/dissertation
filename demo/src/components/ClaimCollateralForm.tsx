abf03e62321e0c7dd8b13a3a129152b5c61df10f
Daniel Izdebski <spidiritsu@gmail.com>
2021-08-08 21:45:57 +0200

    Make ClaimCollateralForm functional

--- demo/src/components/ClaimCollateralForm.tsx
+++ demo/src/components/ClaimCollateralForm.tsx
@@ -5,20 +5,30 @@ import {
   Box,
   Button,
 } from "@chakra-ui/react"
+import { useState } from 'react'
+import { useContractMethod } from '../hooks'
 
 export default function ClaimCollateralForm() {
+  const [orderHash, setOrderHash] = useState('')
+  const handleChange = (event: any) => setOrderHash(event.target.value)
+  const { send } = useContractMethod('claimCollateral')
+
+  const handleAction = async () => {
+    await send(orderHash)
+  }
+
   return (
     <>
       <Stack spacing={3}>
         <Box>
           <Text>Order hash</Text>
-          <Input />
+          <Input value={orderHash} onChange={handleChange} />
         </Box>
         <br />
-        <Button isFullWidth colorScheme="purple">
-          Claim Collateral
+        <Button isFullWidth colorScheme="purple" onClick={handleAction}>
+          Claim collateral
         </Button>
-    </Stack>
+      </Stack>
     </>
   );
 }
