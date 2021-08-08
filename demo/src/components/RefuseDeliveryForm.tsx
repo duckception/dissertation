506720fd0743d665752af583b1fdd58ad3b4c5dc
Daniel Izdebski <spidiritsu@gmail.com>
2021-08-08 21:44:18 +0200

    Make RefuseDeliveryForm functional

--- demo/src/components/RefuseDeliveryForm.tsx
+++ demo/src/components/RefuseDeliveryForm.tsx
@@ -5,17 +5,27 @@ import {
   Box,
   Button,
 } from "@chakra-ui/react"
+import { useState } from 'react'
+import { useContractMethod } from '../hooks'
 
 export default function RefuseDeliveryForm() {
+  const [orderHash, setOrderHash] = useState('')
+  const handleChange = (event: any) => setOrderHash(event.target.value)
+  const { send } = useContractMethod('refuseDelivery')
+
+  const handleAction = async () => {
+    await send(orderHash)
+  }
+
   return (
     <>
       <Stack spacing={3}>
         <Box>
           <Text>Order hash</Text>
-          <Input />
+          <Input value={orderHash} onChange={handleChange} />
         </Box>
         <br />
-        <Button isFullWidth colorScheme="purple">
+        <Button isFullWidth colorScheme="purple" onClick={handleAction}>
           Refuse delivery
         </Button>
     </Stack>
